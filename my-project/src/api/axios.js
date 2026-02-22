1d93c7f8c98e75d88a747339cd6a9ec23ec53ae6
poojasingh5555 <singhpoojareckofficial@gmail.com>
2026-02-22 11:45:53 +0530

    changed the file

--- my-project/src/api/axios.js
+++ my-project/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://expertbook.onrender.com",
+  baseURL: "http://localhost/5000",
 });
 
 export default API;
--- my-project/src/socketio/Socket.js
+++ my-project/src/socketio/Socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://expertbook.onrender.com");
+const socket = io("http://localhost/5000");
 
 export default socket;
 
